62171471c5bea69ab2a1576dc314dab9fee5f473
sourav4243 <souravkumar18835@gmail.com>
2025-07-18 23:19:15 +0530

    flask backend initialized with venv

---
+++ backend/app.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
+from flask import request
+
+app = Flask(__name__)
+# CORS(app)   # This will allow frontend origins by default
+CORS(app, origins=["http://localhost:3000"])    # Restricting to only frontend
+
+# if using nextjs on different port or domain
+# CORS(app, origins=["http://localhost:3000", "https://smartexamai.vercel.app"])
+
+
+
+
+@app.route('/')
+def home():
+    return "Hello Sourav, this is your Flask Backend!"
+
+@app.route('/about')
+def about():
+    return "This is Sourav's about route"
+
+
+@app.route('/api/greet')
+def greet():
+    return jsonify({'message':'Hello from Flask!'})
+
+
+# Handling JSON data from frontend
+@app.route('/upload', method=['POST'])
+def upload():
+    data = request.get_json()  # Read JSON data
+    print("Recieved: ", data)
+
+    # Example: access fields
+    user_id = data.get("user_id")
+    file_urls = data.get("file_urls") # list of urls sent from frontend
+
+    print("User ID: ", user_id)
+    print("Files: ", file_urls)
+
+    return jsonify({"satus":"success", "message": "Data recieved!"})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
